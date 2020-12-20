fecc1831ee971a722a839ff73637d3af51d6d2f9
MKemp <mkemp@accu-time.com>
2020-12-20 18:05:03 -0500

    create retrofit interface and retrofit instance

---
+++ app/src/main/java/com/mkemp/retrofitdemo/AlbumService.kt
@@ -0,0 +1,11 @@
+package com.mkemp.retrofitdemo
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface AlbumService {
+
+    @GET("/albums")
+    suspend fun getAlbums() : Response<Albums>
+
+}
