f2a12b02336799db6b860b9d769d9625115dbbbf
MKemp <mkemp@accu-time.com>
2020-12-21 09:33:51 -0500

    use retrofit to show album data in scrollview

--- app/src/main/java/com/mkemp/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/mkemp/retrofitdemo/MainActivity.kt
@@ -2,6 +2,12 @@ package com.mkemp.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity()
 {
@@ -9,5 +15,27 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+                    Log.i("MyTag", "onCreate: ${albumsItem.title}")
+                    val result =  " " + "Album id : ${albumsItem.id}" + "\n" +
+                            " " + "Album title : ${albumsItem.title}" + "\n" +
+                            " " + "user id : ${albumsItem.userId}" + "\n\n\n"
+                    text_view.append(result)
+                }
+            }
+        })
     }
 }
