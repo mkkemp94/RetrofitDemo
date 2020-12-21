0490fdf74aaefda393fd6048fbbed97332339769
MKemp <mkemp@accu-time.com>
2020-12-21 10:21:08 -0500

    use retrofit query

--- app/src/main/java/com/mkemp/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/mkemp/retrofitdemo/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity()
             .getRetrofitInstance()
             .create(AlbumService::class.java)
         val responseLiveData: LiveData<Response<Albums>> = liveData {
-            val response = retService.getAlbums()
+            val response = retService.getSortedAlbums(3)
             emit(response)
         }
 
